49cdc1ca4072a62c13043179154250d4d663f237
Ian Andwati <andwatiian@gmail.com>
2024-03-06 01:47:05 +0300

    API complete

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,3 @@
-# from django.urls import include, path
-# from rest_framework.routers import DefaultRouter
-
-# from .views import ProductViewSet
-
-# router = DefaultRouter()
-# router.register(r"Products", ProductViewSet)
-# urlpatterns = router.urls
-
 from django.urls import path
 
 from .views import ProductDetailView, ProductList
